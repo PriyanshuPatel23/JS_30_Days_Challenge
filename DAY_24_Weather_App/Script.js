19c6f88b9f43027e62467d57fd97e35da49d1c8c
PriyanshuPatel23 <pkp266276@gmail.com>
2024-09-05 13:44:53 +0530

    code formatted for day 24

--- DAY_24_Weather_App/Script.js
+++ DAY_24_Weather_App/Script.js
@@ -56,12 +56,10 @@ function getForecast(latitude, longitude) {
   fetch(forecastUrl)
     .then((response) => response.json())
     .then((data) => {
-      // Data me se sirf wo readings le lo jo din me ek hi baar ka data ho
       const forecastData = data.list.filter((reading) =>
         reading.dt_txt.includes("12:00:00")
       );
 
-      // Update the forecast cards
       updateForecast(forecastData);
     })
     .catch((error) => console.error("Error fetching weather data:", error));
@@ -71,32 +69,22 @@ function updateForecast(forecastData) {
   const forecastContainer = document.querySelector(".forecast-cards");
   const cardTemplate = document.querySelector(".forecast-card");
 
-  // Card ko clear kar do pehle (sirf ek card chhodein)
   forecastContainer.innerHTML = "";
 
-  // Loop for 5 days data
   forecastData.forEach((dayData, index) => {
-    // Ek naya card clone karo
     const newCard = cardTemplate.cloneNode(true);
 
-    // Date ko convert karo aur din ka naam nikaalo
-    const date = new Date(dayData.dt * 1000); // Unix timestamp ko date me badlo
-    const day = date.toLocaleDateString("en-US", { weekday: "long" }); // Weekday ka naam
-    const temp = Math.round(dayData.main.temp); // Temperature in Celsius
-    const condition = dayData.weather[0].description; // Weather condition description
-    const icon = `http://openweathermap.org/img/wn/${dayData.weather[0].icon}.png`; // Weather icon
+    const date = new Date(dayData.dt * 1000);
+    const day = date.toLocaleDateString("en-US", { weekday: "long" });
+    const temp = Math.round(dayData.main.temp);
+    const condition = dayData.weather[0].description;
+    const icon = `http://openweathermap.org/img/wn/${dayData.weather[0].icon}.png`;
 
-    // Naye card ko data se update karo
-    newCard.querySelector("p:nth-child(1)").innerText = day; // Din ka naam
-    newCard.querySelector("img").src = icon; // Weather icon
-    newCard.querySelector(
-      "p:nth-child(3)"
-    ).innerText = `Temperature: ${temp}°C`; // Temperature
-    newCard.querySelector(
-      "p:nth-child(4)"
-    ).innerText = `Condition: ${condition}`; // Weather condition
+    newCard.querySelector("p:nth-child(1)").innerText = day;
+    newCard.querySelector("img").src = icon;
+    newCard.querySelector("p:nth-child(3)").innerText = `${temp}°C`;
+    newCard.querySelector("p:nth-child(4)").innerText = `${condition}`;
 
-    // Naya card forecastContainer me add karo
     forecastContainer.appendChild(newCard);
   });
 }
